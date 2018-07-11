82f03a43bee85b90353ed2f6fea9ca585160b86a
Ben Anderson <BAnderson@predictivetechnologies.com>
2018-07-11 14:47:19 -0400

    initial commit

---
+++ build/index.d.ts
@@ -0,0 +1,8 @@
+/// <amd-module name="mynamespace::SomeModule" />
+/// <amd-module name="mynamespace::SomeModule" />
+declare module "mynamespace::SomeModule" {
+    export class SillyClass {
+        private readonly _foo;
+        constructor();
+    }
+}
---
+++ src/SomeModule.ts
@@ -0,0 +1,8 @@
+/// <amd-module name="mynamespace::SomeModule" />
+
+export class SillyClass {
+    private readonly _foo: string;
+    constructor() {
+        this._foo = "bar";
+    }
+}
